9280c546996a28a6cd06e89a7dc6616912948c7e
mujtabadustox <i190577@nu.edu.pk>
2023-06-17 19:41:22 +0500

    last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <h1>Hi Mom!</h1>
+import { prisma } from "@/db";
+import Link from "next/link";
+import TodoItems from "./components/TodoItems";
+
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo (id: string , complete : boolean){
+"use server"
+
+await prisma.todo.update({where : {id}, data: {complete}})
+}
+
+export default async function Home() {
+
+
+  // await prisma.todo.create ({data: {title: "test", complete: false}})
+  const todos = await getTodos()  
+  return <>
+  <header className="flex justify-between items-center mb-4">
+  <h1 className="text-2xl">Todos</h1>
+  <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">Add New</Link>
+  </header>
+  <ul className="pl-4">
+{todos.map((todo)=>(
+  <TodoItems key={todo.id} {...todo} toggleTodo={toggleTodo}/>
+))}
+  </ul>
+  </>
 }
